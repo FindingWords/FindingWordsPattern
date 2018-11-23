2c40e1e1f997ac236cc09b046a47a38e47b5bb2e
arturoaxity <artbmz005@gmail.com>
2018-11-22 18:07:54 -0600

    The methods get and set was created in class BookVO.

--- PatronTransferObject/src/mx/edu/uttt/trasnfer/BookVO.java
+++ PatronTransferObject/src/mx/edu/uttt/trasnfer/BookVO.java
@@ -5,10 +5,78 @@
  */
 package mx.edu.uttt.trasnfer;
 
+import java.util.Date;
+
 /**
  *
  * @author galaxias
  */
 public class BookVO {
+  private int idbook;
+  private String title;
+private String autor;
+private String editorial;
+private Date dateed;
+private int page; 
+private String categorie;
+
+    public int getIdbook() {
+        return idbook;
+    }
+
+    public void setIdbook(int idbook) {
+        this.idbook = idbook;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getEditorial() {
+        return editorial;
+    }
+
+    public void setEditorial(String editorial) {
+        this.editorial = editorial;
+    }
+
+    public Date getDateed() {
+        return dateed;
+    }
+
+    public void setDateed(Date dateed) {
+        this.dateed = dateed;
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public String getCategorie() {
+        return categorie;
+    }
+
+    public void setCategorie(String categorie) {
+        this.categorie = categorie;
+    }
+
+    
+    
     
 }
